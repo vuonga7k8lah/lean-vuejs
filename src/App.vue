cd0bc6aeeee94bb8eeb288ff4600202fb7fd8e0e
vuonga7k8lah <vuonga7k8lnc@gmail.com>
2021-09-07 23:09:55 +0700

    init code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu/>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/Menu.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Menu,
   }
 }
 </script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+    <h1 v-show="isShow">Đây Là Menu</h1>
+    <button @click="isShow =! isShow">An/Hien</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            isShow:true
+        };
+    }
+}
+</script>
+
+<style scoped>
+</style>
